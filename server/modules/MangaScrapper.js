fb6707a1f66070c7dc29aeecd7cf1b2672d37e86
Myron <dubtwee@gmail.com>
2020-03-03 11:00:03 -0600

    moved all initial scrapper test code to a method on the MangaScrapper class

--- server/modules/MangaScrapper.js
+++ server/modules/MangaScrapper.js
@@ -11,6 +11,83 @@ class MangaScapper {
         this._searchWords = null;
     }
 
+    _init() {
+        this._loadScan();
+    }
+
+    async _loadScan() {
+        // open the headless browser
+        // to run full version of chrome use { headless: false } as launch argument
+        const browser = await puppeteer.launch();
+        // logger.sample('SERVER, browser:', browser);
+        // open a new browser page
+        const page = await browser.newPage();
+        // logger.sample('SERVER, page:', page);
+        // enter url in page and navigate to that page
+        // Navigates to the search results for the 
+        await page.goto(`https://manganelo.com/search/solo_leveling`);
+
+        logger.label('PAGE LOADED');
+        
+        const searchResultSelector = `#SearchResult`;
+        const searchInputSelector = `#search-story`;
+        const searchResultItem = `.search-story-item`;
+        const searchResultDropdownLinks = `#SearchResult ul > a`;
+    
+        const resultsDataList = await page.evaluate(() => {
+            const searchResults = document.querySelectorAll(`.search-story-item > a`);
+            const resultsList = [];
+            searchResults.forEach((item) => {
+                resultsList.push({
+                    path: item.href,
+                    title: item.title,
+                });
+            });
+    
+            return Promise.resolve(resultsList);
+        });
+        console.log(resultsDataList);
+    
+        await page.goto(resultsDataList[0].path);
+        const mangaChapterList = await page.evaluate(() => {
+            const chapterLinks = document.querySelectorAll('.row-content-chapter > li > a.chapter-name');
+            const chapterDataList = [];
+            chapterLinks.forEach((item) => {
+                chapterDataList.push({
+                    path: item.href,
+                    chapterName: item.text,
+                    title: item.title,
+                });
+            });
+    
+            return Promise.resolve(chapterDataList);
+        });
+        console.log(mangaChapterList);
+    
+        // mangaChapterList.forEach(async (item, index) => {
+        //     const chapterPath = item.path;
+        //     await page.goto(chapterPath);
+        //     const chapterImagesData = await page.evaluate(() => {
+        //         const chapterImageElem = document.querySelectorAll('.container-chapter-reader > img');
+        //         const chapterImageList = [];
+        //         chapterImageElem.forEach((item) => {
+        //             chapterImageList.push({
+        //                 imgSrc: item.scroll,
+        //                 title: item.title,
+        //             });
+        //         });
+    
+        //         return Promise.resolve(chapterImageList);
+        //     });
+    
+        //     mangaChapterList[index].images = chapterImagesData;
+        // });
+    
+        await browser.close();
+    }
+
+    
+
     _makeSearchTermParam(searchTerm) {
         let searchParam = searchTerm.replace(' ', '_');
         return searchParam.toLowerCase();
@@ -41,74 +118,5 @@ const searchWord = 'Solo Leveling';
 
 // tutorial
 // https://blog.bitsrc.io/web-scraping-with-puppeteer-e73e5fee7474
-(async () => {
-    // open the headless browser
-    // to run full version of chrome use { headless: false } as launch argument
-    const browser = await puppeteer.launch();
-    // logger.sample('SERVER, browser:', browser);
-    // open a new browser page
-    const page = await browser.newPage();
-    // logger.sample('SERVER, page:', page);
-    // enter url in page and navigate to that page
-    await page.goto(`https://manganelo.com/search/solo_leveling`);
-
-    logger.label('PAGE LOADED');
-
-    const searchResultSelector = `#SearchResult`;
-    const searchInputSelector = `#search-story`;
-    const searchResultItem = `.search-story-item`;
-    const searchResultDropdownLinks = `#SearchResult ul > a`;
-
-    const resultsDataList = await page.evaluate(() => {
-        const searchResults = document.querySelectorAll(`.search-story-item > a`);
-        const resultsList = [];
-        searchResults.forEach((item) => {
-            resultsList.push({
-                path: item.href,
-                title: item.title,
-            });
-        });
-
-        return Promise.resolve(resultsList);
-    });
-    console.log(resultsDataList);
-
-    await page.goto(resultsDataList[0].path);
-    const mangaChapterList = await page.evaluate(() => {
-        const chapterLinks = document.querySelectorAll('.row-content-chapter > li > a.chapter-name');
-        const chapterDataList = [];
-        chapterLinks.forEach((item) => {
-            chapterDataList.push({
-                path: item.href,
-                chapterName: item.text,
-                title: item.title,
-            });
-        });
-
-        return Promise.resolve(chapterDataList);
-    });
-    console.log(mangaChapterList);
-
-    // mangaChapterList.forEach(async (item, index) => {
-    //     const chapterPath = item.path;
-    //     await page.goto(chapterPath);
-    //     const chapterImagesData = await page.evaluate(() => {
-    //         const chapterImageElem = document.querySelectorAll('.container-chapter-reader > img');
-    //         const chapterImageList = [];
-    //         chapterImageElem.forEach((item) => {
-    //             chapterImageList.push({
-    //                 imgSrc: item.scroll,
-    //                 title: item.title,
-    //             });
-    //         });
-
-    //         return Promise.resolve(chapterImageList);
-    //     });
-
-    //     mangaChapterList[index].images = chapterImagesData;
-    // });
-
-    await browser.close();
-})();
 
 module.exports = MangaScapper.createSingleton();
