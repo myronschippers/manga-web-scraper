9c3dfba51c508d964005db367803b43cdd2148a0
Myron <dubtwee@gmail.com>
2020-07-17 20:03:49 -0500

    adjusting so that the code styling is using 2 space indentation

--- server/routes/scapper.router.js
+++ server/routes/scapper.router.js
@@ -8,51 +8,52 @@ const mangaSeriesDb = require('../services/MangaSeriesDb');
 const logger = require('../utilities/logger');
 
 /**
- * GET route template
+ * GET route for pulling all of the saved manga series
  */
 router.get('/chapters/:seriesId', (req, res, next) => {
-    // const seriesId = req.params.seriesId;
-    mangaSeriesDb.fetchSeries(req.params.seriesId)
+  // const seriesId = req.params.seriesId;
+  mangaSeriesDb.fetchSeries(req.params.seriesId)
+    .then((response) => {
+      const matchedSeries = response.rows[0];
+      mangaScraper.chaptersForSeries(matchedSeries)
         .then((response) => {
-            const matchedSeries = response.rows[0];
-            mangaScraper.chaptersForSeries(matchedSeries)
-              .then((response) => {
-                  res.send(response);
-              })
-              .catch((err) => {
-                  console.log('Error scraping series chapters:', err);
-                  res.sendStatus(500);
-              });
+          res.send(response);
         })
         .catch((err) => {
-            console.log('Error fetching series chapters:', err);
-            res.sendStatus(500);
+          console.log('Error scraping series chapters:', err);
+          res.sendStatus(500);
         });
+    })
+    .catch((err) => {
+      console.log('Error fetching series chapters:', err);
+      res.sendStatus(500);
+    });
 });
 
 /**
- * POST route template
+ * POST route that searches for a manga series
+ * based on key term sent
  */
 router.post('/search', (req, res, next) => {
-    const {
-        term,
-    } = req.body;
+  const {
+    term,
+  } = req.body;
 
-    mangaScraper.search(term)
-        .then((results) => {
-            logger.success('POST /api/scrape/search:', results);
+  mangaScraper.search(term)
+    .then((results) => {
+      logger.success('POST /api/scrape/search:', results);
 
-            res.status(201)
-            res.send(results);
-        })
-        .catch((err) => {
-            logger.error('POST /api/scrape/search:', err);
+      res.status(201)
+      res.send(results);
+    })
+    .catch((err) => {
+      logger.error('POST /api/scrape/search:', err);
 
-            res.status(500);
-            res.send({
-                message: err
-            });
-        });
+      res.status(500);
+      res.send({
+        message: err
+      });
+    });
 });
 
 module.exports = router;
