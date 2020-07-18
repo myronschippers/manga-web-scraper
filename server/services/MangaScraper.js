7c719bce59eee8193aceb4d51eed926997146d62
Myron <dubtwee@gmail.com>
2020-07-17 20:27:37 -0500

    altering tabbing to be 2 spaces instead of 4

--- server/services/MangaScraper.js
+++ server/services/MangaScraper.js
@@ -4,201 +4,209 @@ const puppeteer = require('puppeteer');
 const logger = require('../utilities/logger');
 
 class MangaScraper {
-    constructor() {
-        this._baseUrl = 'https://manganelo.com/';
-        this._searchParam = 'search/';
-        this._searchWords = null;
-        this._headlessChrome = {};
-        this._isLoaded = false;
-    }
+  constructor() {
+    this._baseUrl = 'https://manganelo.com/';
+    this._searchParam = 'search/';
+    this._searchWords = null;
+    this._headlessChrome = {};
+    this._isLoaded = false;
+  }
 
-    _init() {
-        this._loadScan().then((scanner) => {
-            this._headlessChrome = scanner;
+  _init() {
+    this._loadScan().then((scanner) => {
+      this._headlessChrome = scanner;
+    });
+  }
+
+  async _loadScan() {
+    // open the headless browser
+    // to run full version of chrome use { headless: false } as launch argument
+    const browser = await puppeteer.launch();
+    // logger.sample('SERVER, browser:', browser);
+    // open a new browser page
+    const page = await browser.newPage();
+    // logger.sample('SERVER, page:', page);
+
+    this._headlessChrome = {
+      page,
+      browser
+    };
+    this._isLoaded = true;
+  }
+
+  async _searchMangaSite(formattedSearchTerm) {
+    const {
+      page,
+    } = this._headlessChrome;
+
+    // enter url in page and navigate to that page
+    // Navigates to the search results for the 
+    await page.goto(`https://manganelo.com/search/story/${formattedSearchTerm}`);
+
+    logger.label('SEARCH PAGE LOADED');
+    logger.message(formattedSearchTerm);
+
+    const resultsDataList = await page.evaluate(() => {
+      const searchResults = document.querySelectorAll(`.search-story-item > a`);
+      const resultsList = [];
+
+      searchResults.forEach((item) => {
+        const elemSearchItem = item.parentElement;
+        const elemAuthor = elemSearchItem.querySelector('.item-author');
+        let author = elemAuthor != null ? elemAuthor.title : 'not available';
+
+        resultsList.push({
+          path: item.href,
+          title: item.title,
+          thumbnail: item.children[0] != null ? item.children[0].src : null,
+          author
         });
-    }
+      });
 
-    async _loadScan() {
-        // open the headless browser
-        // to run full version of chrome use { headless: false } as launch argument
-        const browser = await puppeteer.launch();
-        // logger.sample('SERVER, browser:', browser);
-        // open a new browser page
-        const page = await browser.newPage();
-        // logger.sample('SERVER, page:', page);
+      return Promise.resolve(resultsList);
+    });
 
-        this._headlessChrome = {
-            page,
-            browser
+    return resultsDataList;
+
+    // await browser.close();
+  }
+
+  async _scrapeChapterList(mangaData) {
+    const {
+      page
+    } = this._headlessChrome;
+
+    // SEARCH THUMBNAIL >> GOTO RESULT PAGE FOR CHAPTERS
+
+    await page.goto(mangaData.path);
+    const mangaChapterList = await page.evaluate(() => {
+      const chapterLinks = document.querySelectorAll('.row-content-chapter > li > a.chapter-name');
+      const chapterDataList = [];
+      chapterLinks.forEach((item) => {
+        const rawResultItemData = {
+          path: item.href,
+          chapterName: item.text,
+          title: item.title,
         };
-        this._isLoaded = true;
-    }
 
-    async _searchMangaSite(formattedSearchTerm) {
-        const {
-            page,
-        } = this._headlessChrome;
+        chapterDataList.push(rawResultItemData);
+      });
 
-        // enter url in page and navigate to that page
-        // Navigates to the search results for the 
-        await page.goto(`https://manganelo.com/search/story/${formattedSearchTerm}`);
+      return Promise.resolve(chapterDataList);
+    });
 
-        logger.label('SEARCH PAGE LOADED');
-        logger.message(formattedSearchTerm);
+    return mangaChapterList;
+  }
 
-        const resultsDataList = await page.evaluate(() => {
-            const searchResults = document.querySelectorAll(`.search-story-item > a`);
-            const resultsList = [];
+  async _scrapePagesForChapter(mangaChapterList) {
+    const {
+      page
+    } = this._headlessChrome;
 
-            searchResults.forEach((item) => {
-                const elemSearchItem = item.parentElement;
-                const elemAuthor = elemSearchItem.querySelector('.item-author');
-                let author = elemAuthor != null ? elemAuthor.title : 'not available';
-
-                resultsList.push({
-                    path: item.href,
-                    title: item.title,
-                    thumbnail: item.children[0] != null ? item.children[0].src : null,
-                    author
-                });
-            });
-
-            return Promise.resolve(resultsList);
+    mangaChapterList.forEach(async (item, index) => {
+      const chapterPath = item.path;
+      await page.goto(chapterPath);
+      const chapterImagesData = await page.evaluate(() => {
+        const chapterImageElem = document.querySelectorAll('.container-chapter-reader > img');
+        const chapterImageList = [];
+        chapterImageElem.forEach((item) => {
+          chapterImageList.push({
+            imgSrc: item.scroll,
+            title: item.title,
+          });
         });
 
-        return resultsDataList;
+        return Promise.resolve(chapterImageList);
+      });
 
-        // await browser.close();
+      mangaChapterList[index].images = chapterImagesData;
+    });
+  }
+
+  async _closeScanner() {
+    const {
+      browser,
+    } = this._headlessChrome;
+
+    await browser.close();
+
+    this._headlessChrome = {};
+    this._isLoaded = false;
+  }
+
+  _makeSearchTermParam(searchTerm) {
+    let searchParam = searchTerm.replace(' ', '_');
+    return searchParam.toLowerCase();
+  }
+
+  async _checkBrowser() {
+    if (!this._isLoaded) {
+      await this._loadScan();
     }
+  }
 
-    async _scrapeChapterList(mangaData) {
-        const {
-            page
-        } = this._headlessChrome;
+  //
+  // PUBLIC METHODS
+  // ------------------------------
 
-        // SEARCH THUMBNAIL >> GOTO RESULT PAGE FOR CHAPTERS
+  async search(searchWords) {
+    try {
+      // logger.label('SEARCHING');
+      this._searchWords = searchWords;
+      // logger.message('searchWords:', searchWords);
+      const searchFormatted = this._makeSearchTermParam(searchWords);
+      // logger.message('searchFormatted:', searchFormatted);
 
-        await page.goto(mangaData.path);
-        const mangaChapterList = await page.evaluate(() => {
-            const chapterLinks = document.querySelectorAll('.row-content-chapter > li > a.chapter-name');
-            const chapterDataList = [];
-            chapterLinks.forEach((item) => {
-                const rawResultItemData = {
-                    path: item.href,
-                    chapterName: item.text,
-                    title: item.title,
-                };
+      await this._checkBrowser();
 
-                chapterDataList.push(rawResultItemData);
-            });
+      const searchResults = await this._searchMangaSite(searchFormatted);
 
-            return Promise.resolve(chapterDataList);
-        });
+      await this._closeScanner();
 
-        return mangaChapterList;
+      // logger.message('SEARCH RESULTS:');
+      // console.log(searchResults)
+      // logger.end('SEARCHING');
+      return searchResults;
+    } catch(err) {
+      throw(err);
     }
+  }
 
-    async _scrapePagesForChapter(mangaChapterList) {
-        const {
-            page
-        } = this._headlessChrome;
+  async chaptersForSeries(seriesData) {
+    try {
+      await this._checkBrowser();
 
-        mangaChapterList.forEach(async (item, index) => {
-            const chapterPath = item.path;
-            await page.goto(chapterPath);
-            const chapterImagesData = await page.evaluate(() => {
-                const chapterImageElem = document.querySelectorAll('.container-chapter-reader > img');
-                const chapterImageList = [];
-                chapterImageElem.forEach((item) => {
-                    chapterImageList.push({
-                        imgSrc: item.scroll,
-                        title: item.title,
-                    });
-                });
+      const chapterCollection = await this._scrapeChapterList(seriesData);
 
-                return Promise.resolve(chapterImageList);
-            });
+      await this._closeScanner();
 
-            mangaChapterList[index].images = chapterImagesData;
-        });
+      return chapterCollection;
+    } catch(errSeries) {
+      throw(errSeries);
     }
+  }
 
-    async _closeScanner() {
-        const {
-            browser,
-        } = this._headlessChrome;
+  // TODO: Need to make function get all of the pages of a single chapter
+  async pagesForChapter(chapterData) {
+    // await this._checkBrowser();
 
-        await browser.close();
+    // const chapterUrl = chapterData.path;
+    // const pagesResults = await this._extractPageImagesForChapter(chapterUrl);
+    // // TODO: Create _extractPageImagesForChapter
 
-        this._headlessChrome = {};
-        this._isLoaded = false;
-    }
+    // await this._closeScanner();
+  }
 
-    _makeSearchTermParam(searchTerm) {
-        let searchParam = searchTerm.replace(' ', '_');
-        return searchParam.toLowerCase();
-    }
+  // TODO: Need to make the function get all pages for all chapters
+  async pagesForChapterCollection(chapterCollection) {}
 
-    async _checkBrowser() {
-        if (!this._isLoaded) {
-            await this._loadScan();
-        }
-    }
+  //
+  // KICKOFF FOR SINGLETON
+  // ------------------------------
 
-    //
-    // PUBLIC METHODS
-    // ------------------------------
-
-    async search(searchWords) {
-        try {
-            // logger.label('SEARCHING');
-            this._searchWords = searchWords;
-            // logger.message('searchWords:', searchWords);
-            const searchFormatted = this._makeSearchTermParam(searchWords);
-            // logger.message('searchFormatted:', searchFormatted);
-
-            await this._checkBrowser();
-
-            const searchResults = await this._searchMangaSite(searchFormatted);
-
-            await this._closeScanner();
-
-            // logger.message('SEARCH RESULTS:');
-            // console.log(searchResults)
-            // logger.end('SEARCHING');
-            return searchResults;
-        } catch(err) {
-            throw(err);
-        }
-    }
-
-    async chaptersForSeries(seriesData) {
-        try {
-            await this._checkBrowser();
-
-            const chapterCollection = await this._scrapeChapterList(seriesData);
-
-            await this._closeScanner();
-
-            return chapterCollection;
-        } catch(errSeries) {
-            throw(errSeries);
-        }
-    }
-
-    // TODO: Need to make function get all of the pages of a single chapter
-    async pagesForChapter(chapterData) {}
-
-    // TODO: Need to make the function get all pages for all chapters
-    async pagesForChapterCollection(chapterCollection) {}
-
-    //
-    // KICKOFF FOR SINGLETON
-    // ------------------------------
-
-    static createSingleton() {
-        return new MangaScraper();
-    }
+  static createSingleton() {
+    return new MangaScraper();
+  }
 }
 
 // tutorial
