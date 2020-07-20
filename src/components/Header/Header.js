385668a9673cf41f19fda57093f028d2605de223
Myron <dubtwee@gmail.com>
2020-07-19 19:43:22 -0500

    addjust the layout of the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import styles from './Header.module.css';
 import Navigation from '../Navigation/Navigation';
+import SearchField from '../SearchField/SearchField';
 
 class Header extends Component {
   render() {
     return (
       <header className={styles.appBar}>
-        <h1 className={styles.appBar_hdg}>{this.props.primeHdg}</h1>
-        <Navigation />
+        <div className={styles['appBar-primary']}>
+          <h1 className={styles.appBarHdg}>{this.props.primeHdg}</h1>
+        </div>
+        <div className={styles['appBar-block']}>
+          <SearchField type="redirect" />
+        </div>
+        <div className={styles['appBar-block']}>
+          <Navigation />
+        </div>
       </header>
     );
   }
