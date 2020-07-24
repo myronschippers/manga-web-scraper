42368ab7d0aea592873784cbfab9b267582dbc1a
Myron <dubtwee@gmail.com>
2020-07-24 16:21:11 -0500

    altering to 2 space tabbing

--- src/redux/mapStoreToProps.js
+++ src/redux/mapStoreToProps.js
@@ -1,19 +1,19 @@
 const mapStoreToProps = (...keyList) => ((store) => {
-    let specificStore = store;
+  let specificStore = store;
 
-    if (keyList != null && keyList.length !== 0) {
-        specificStore = {};
+  if (keyList != null && keyList.length !== 0) {
+    specificStore = {};
 
-        for (let reducerKey of keyList) {
-            if (store[reducerKey] != null) {
-                specificStore[reducerKey] = store[reducerKey];
-            }
-        }
+    for (let reducerKey of keyList) {
+      if (store[reducerKey] != null) {
+        specificStore[reducerKey] = store[reducerKey];
+      }
     }
+  }
 
-    return {
-        store: specificStore,
-    };
+  return {
+    store: specificStore,
+  };
 });
 
 export default mapStoreToProps;
